5e74d5b896415c3e152ef2fed2a837a4b33c13c4
Douglas <douglas.159@hotmail.com>
2023-07-30 17:41:07 -0300

    primeiro commit

--- src/app/components/card-box/card-box.component.ts
+++ src/app/components/card-box/card-box.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountDataModel } from 'src/app/model/accountDataModel';
 
 @Component({
   selector: 'app-card-box',
   templateUrl: './card-box.component.html',
   styleUrls: ['./card-box.component.css']
 })
-export class CardBoxComponent {
+export class CardBoxComponent implements OnInit {
+  
+  accountData: AccountDataModel = {
+    name: "Darth Vader",
+    account: {
+      agency: "0000",
+      number: "00.000000-0" 
+    },
+    card: {
+      limit: 0,
+      number: "0000"
+    }
+  }
 
+  ngOnInit(): void {
+    alert ("Ola mundo")
+  }
 }
---
+++ src/app/services/cards.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../environments/environments';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardsService {
+  
+  private apiURL = environment.API_URL
+
+  constructor(private http: HttpClient) { }
+
+  //getCard(): Observable<any>{}
+}
